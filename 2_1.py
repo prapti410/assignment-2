2ea2f5c68ac24673eeac92b0f253c18b54e64f57
prapti410 <64392255+prapti410@users.noreply.github.com>
2020-08-30 07:47:37 -0700

    Add files via upload

---
+++ 2_1.py
@@ -0,0 +1,9 @@
+radius=int(input("enter the radius of sphere"))
+pi=3.14
+
+def vol_calc(radius):
+    vol_of sphere= (4*pi*(radius**3)/3
+    return vol_of sphere
+
+
+    print("the volume of sphere of radius {radius} is {vol_calc (radius):4}cubic units")
